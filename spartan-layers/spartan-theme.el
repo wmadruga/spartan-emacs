4090ef0e96e78f54307a3830400e8494d2e6a3a2
William Madruga <willmadruga@gmail.com>
2021-02-27 14:10:13 -0800

    Added doom-modeline to pair with the doom-one theme :)

--- spartan-layers/spartan-theme.el
+++ spartan-layers/spartan-theme.el
@@ -1,6 +1,12 @@
-;;; -*- lexical-binding: t; no-byte-compile: t; -*-
+;;; spartan-theme --- Describe theme packages  -*- lexical-binding: t; no-byte-compile: t; -*-
 
+;;; Commentary:
+;; Describe theme packages but does not load.  Loading happens later on, see init.el.
+
+;;; Code:
 (add-to-list 'spartan-package-list 'doom-themes)
+(add-to-list 'spartan-package-list 'doom-modeline)
 ;; selected doom theme to be loaded later, via symlink
 
 (provide 'spartan-theme)
+;;; spartan-theme.el ends here
